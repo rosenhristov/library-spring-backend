27a00bd50c777f92ed631d77bf980bdf30b837b0
rhristov <rosenhrist@gmail.com>
2023-03-28 17:00:01 +0300

    Removed react from this repository

--- src/layouts/HomePage/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-import {ExploreTopBooks} from "./components/ExploreTopBooks";
-
-export const HomePage = () => {
-    return (
-        <div>
-            <ExploreTopBooks/>
-        </div>
-    )
-}
